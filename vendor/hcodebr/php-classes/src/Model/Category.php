0a0117cc89a57941c58e01273185910d73ecd268
GustavoLenine97 <gustavolkjh_@hotmail.com>
2020-05-21 17:01:13 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -103,6 +103,32 @@ class Category extends Model
         }
     }
 
+    public function getProductsPage($page = 1, $itemsPerPage = 3)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_products a 
+            INNER JOIN tb_productscategories b on a.idproduct = b.idproduct
+            INNER JOIN tb_categories c on c.idcategory = b.idcategory 
+            WHERE c.idcategory = :idcategory
+            limit $start, $itemsPerPage;
+        ", [
+            ':idcategory'=>$this->getidcategory()
+        ]);
+
+        $resultTotal = $sql->select("select FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>Product::checkList($results),
+            'total'=>(int)$resultTotal[0]["nrtotal"],
+            'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+        ];
+    }
+
     public function addProduct(Product $product)
     {
         $sql = new Sql();
@@ -122,6 +148,8 @@ class Category extends Model
             ':idproduct'=>$product->getidproduct()
         ]);
     }
+
+
 
 }
 
